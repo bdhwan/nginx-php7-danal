a076c19c259b395ff5263ec4cc55b81a59440507
bdhwan <bdhwan@gmail.com>
2019-11-01 16:49:20 +0900

    fix

--- check.sh
+++ check.sh
@@ -11,28 +11,28 @@ cd /var/www/html/card/inc
 ls
 echo 'will set env'
 
-sed -i -e "s/vvDN_CREDIT_URL/"$DN_CREDIT_URL"/g" conf.php
+# sed -i -e "s/vvDN_CREDIT_URL/"$DN_CREDIT_URL"/g" conf.php
 sed -i -- 's#vvDN_CREDIT_URL#'"$DN_CREDIT_URL"'#g' conf.php
 
-sed -i -e "s/vvPI_SERVER/"$API_SERVER"/g" conf.php
+# sed -i -e "s/vvPI_SERVER/"$API_SERVER"/g" conf.php
 sed -i -- 's#vvPI_SERVER#'"$API_SERVER"'#g' conf.php
 
-sed -i -e "s/vvUSER_SERVER_URL/"$USER_SERVER_URL"/g" conf.php
+# sed -i -e "s/vvUSER_SERVER_URL/"$USER_SERVER_URL"/g" conf.php
 sed -i -- 's#vvUSER_SERVER_URL#'"$USER_SERVER_URL"'#g' conf.php
 
-sed -i -e "s/vvRETURNURL/"$RETURNURL"/g" conf.php
+# sed -i -e "s/vvRETURNURL/"$RETURNURL"/g" conf.php
 sed -i -- 's#vvRETURNURL#'"$RETURNURL"'#g' conf.php
 
-sed -i -e "s/vvCANCELURL/"$CANCELURL"/g" conf.php
+# sed -i -e "s/vvCANCELURL/"$CANCELURL"/g" conf.php
 sed -i -- 's#vvCANCELURL#'"$CANCELURL"'#g' conf.php
 
-sed -i -e "s/vvCPID/"$CPID"/g" conf.php
+# sed -i -e "s/vvCPID/"$CPID"/g" conf.php
 sed -i -- 's#vvCPID#'"$CPID"'#g' conf.php
 
-sed -i -e "s/vvCRYPTOKEY/"$CRYPTOKEY"/g" conf.php
+# sed -i -e "s/vvCRYPTOKEY/"$CRYPTOKEY"/g" conf.php
 sed -i -- 's#vvCRYPTOKEY#'"$CRYPTOKEY"'#g' conf.php
 
-sed -i -e "s/vvIVKEY/"$IVKEY"/g" conf.php
+# sed -i -e "s/vvIVKEY/"$IVKEY"/g" conf.php
 sed -i -- 's#vvIVKEY#'"$CRYPTOKEY"'#g' conf.php
 
 echo 'done set env'
