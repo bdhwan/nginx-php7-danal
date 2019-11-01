0f0916d1e7c611dd4c6851e30a4219cfec9b54cb
bdhwan <bdhwan@gmail.com>
2019-11-01 16:02:59 +0900

    init

---
+++ check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "GIT_URL: "$GIT_URL
+git clone "$GIT_URL"
+echo 'done clone'
+
+rm -rf /var/www/html
+
+mv "$(basename "$GIT_URL" .git)" /var/www/html
+
+cd /var/www/html/card/inc
+ls
+echo 'will set env'
+
+sed -i -e "s/vvDN_CREDIT_URL/"$DN_CREDIT_URL"/g" conf.php
+sed -i -e "s/vvPI_SERVER/"$API_SERVER"/g" conf.php
+sed -i -e "s/vvUSER_SERVER_URL/"$USER_SERVER_URL"/g" conf.php
+sed -i -e "s/vvRETURNURL/"$RETURNURL"/g" conf.php
+sed -i -e "s/vvCANCELURL/"$CANCELURL"/g" conf.php
+sed -i -e "s/vvCPID/"$CPID"/g" conf.php
+sed -i -e "s/vvCRYPTOKEY/"$CRYPTOKEY"/g" conf.php
+sed -i -e "s/vvIVKEY/"$IVKEY"/g" conf.php
+echo 'done set env'
+
+cd /var/www/html
+service php7.0-fpm start && nginx -g 'daemon off;'
+
---
+++ docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build  --no-cache --tag bdhwan/nginx-php7-danal:0.0.1 .
+docker push bdhwan/nginx-php7-danal:0.0.1
