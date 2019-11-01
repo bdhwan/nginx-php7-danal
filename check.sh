5f901c0e88f848876f8f0f587a55cf4bb5a251f1
bdhwan <bdhwan@gmail.com>
2019-11-01 16:48:32 +0900

    fix

--- check.sh
+++ check.sh
@@ -37,21 +37,6 @@ sed -i -- 's#vvIVKEY#'"$CRYPTOKEY"'#g' conf.php
 
 echo 'done set env'
 
-VALUE1=http://111.22.33.4/test
-VALUE2=${VALUE1//./\\.}  #”.” 을 “\.”으로 치환
-
-sed -i -e "s/vvDN_CREDIT_URL/"${${DN_CREDIT_URL//./\\.}//\//\\/}"/g" conf.php
-
- sed -i -- 's#vvDN_CREDIT_URL#'"$DN_CREDIT_URL"'#g' conf.php
-
-VALUE2=${ VALUE2//\//\\/}  #”/” 를 “\/” 으로 치환
-
-sed -e 's/\/.*//' -e 's/ABC.*/ABC='${VALUE2}'/g' /etc/dimark/dimclient.conf ./test.conf > ./test.conf #정상적인 URL String이 추가된다.
-
-
-
-출처: https://commable.tistory.com/entry/sed-사용-시-참고사항 [Lunatic World]
-
 
 cd /var/www/html
 service php7.0-fpm start && nginx -g 'daemon off;'
